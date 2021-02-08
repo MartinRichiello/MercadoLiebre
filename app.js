f5f58bc6eef5243cd148f518b94a29dadc6c2afb
MartinRichiello <martinrichiello@hotmail.com>
2021-02-08 15:52:25 -0300

    Checkpoint

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+
+app.use(express.static('public'));
+
+
+app.listen(4000, ()=>{
+    console.log('Servidor  4000 funcionando');
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
